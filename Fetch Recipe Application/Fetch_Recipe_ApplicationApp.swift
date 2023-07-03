882ee9b872925cbf1955b1b8d322f237eae24d7f
alexshwe1 <alexander.shwe@gmail.com>
2023-07-02 21:14:53 -0700

    Initial Commit

---
+++ Fetch Recipe Application/Fetch_Recipe_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Recipe_ApplicationApp.swift
+//  Fetch Recipe Application
+//
+//  Created by Alex Shwe on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Recipe_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
